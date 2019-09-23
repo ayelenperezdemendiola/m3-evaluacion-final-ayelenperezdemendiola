beb96a1911538f5ad024887f297d33bf1ebe6da2
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-09-23 13:50:18 +0200

    docs

---
+++ docs/precache-manifest.1bed75fe934543e6d6d142c4ed5d436c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c950262b4b9d96c5afedfbe85a55af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f1f2701600a7c6b0b83",
+    "url": "./static/css/main.851f2012.chunk.css"
+  },
+  {
+    "revision": "74d4d1b710afcd353761",
+    "url": "./static/js/2.8526735e.chunk.js"
+  },
+  {
+    "revision": "6f1f2701600a7c6b0b83",
+    "url": "./static/js/main.854422c1.chunk.js"
+  },
+  {
+    "revision": "4c133a058235f06a08d7",
+    "url": "./static/js/runtime-main.c51afb8e.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);

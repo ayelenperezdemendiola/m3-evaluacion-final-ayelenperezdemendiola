2b3e88ad29515af809c43a4595d7be950b181a43
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-09-23 13:20:48 +0200

    visual for pc ok

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -12,20 +12,20 @@ const CharacterDetail = (props) => {
         <React.Fragment>
             <Header />
             <div className="detail--general--container">
-                <Link className="link--back" to="/"><p className="back--add">{`< Volver`}</p></Link>
                 {myCharacter ?
                     <div className="detail--container">
+                        <Link className="link--back" to="/"><p className="back--add">{`< Volver`}</p></Link>
                         <div className="detail--img__container">
                             <img alt={myCharacter.name} src={myCharacter.image} className="detail--img" />
                         </div>
                         <div className="character--description">
-                        <h2 className="detail--name">{myCharacter.name}</h2>
-                        <ul className="detail--info">
-                            <li className="info species"> Especie: {myCharacter.species === 'Human' ? <i className="fas fa-user-circle" /> : <i class="fab fa-reddit-alien" />}</li>
-                            <li className="info planet">{`Planeta: ${myCharacter.origin.name}`}</li>
-                            <li className="info episodes">{`Cantidad de episodios: ${myCharacter.episode.length}`}</li>
-                            <li className="info life"> Estado: {myCharacter.status === 'Alive' ? <i class="fas fa-heart" /> : <i class="fas fa-dizzy" />}</li>
-                        </ul>
+                            <h2 className="detail--name">{myCharacter.name}</h2>
+                            <ul className="detail--info">
+                                <li className="info species"> Especie: {myCharacter.species === 'Human' ? <i className="fas fa-user-circle" /> : <i className="fab fa-reddit-alien" />}</li>
+                                <li className="info planet">{`Planeta: ${myCharacter.origin.name}`}</li>
+                                <li className="info episodes">{`Cantidad de episodios: ${myCharacter.episode.length}`}</li>
+                                <li className="info life"> Estado: {myCharacter.status === 'Alive' ? <i className="fas fa-heart" /> : <i className="fas fa-dizzy" />}</li>
+                            </ul>
                         </div>
                     </div>
                     :
